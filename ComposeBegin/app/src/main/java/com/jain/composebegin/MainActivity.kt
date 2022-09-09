7111abf01dfb003c7acb209dbab101fa0a66b4b3
preritjn <preritbgr3010@gmail.com>
2022-09-09 16:58:41 +0530

    Added some lines

--- ComposeBegin/app/src/main/java/com/jain/composebegin/MainActivity.kt
+++ ComposeBegin/app/src/main/java/com/jain/composebegin/MainActivity.kt
@@ -4,26 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import com.jain.composebegin.ui.theme.ComposeBeginTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Row(modifier = Modifier
-                .fillMaxSize()
+                .width(250.dp)
+                .fillMaxHeight(0.7f)
                 .background(Color.Gray),
                 horizontalArrangement = Arrangement.SpaceAround,
                 verticalAlignment = Alignment.CenterVertically
@@ -35,4 +29,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
