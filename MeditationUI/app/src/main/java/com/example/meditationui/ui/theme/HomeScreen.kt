7bf76cde34d5484000b875d265c8fda8169bb535
preritjn <preritbgr3010@gmail.com>
2023-01-08 21:17:50 +0530

    Added icons,fonts and colors

---
+++ MeditationUI/app/src/main/java/com/example/meditationui/ui/theme/HomeScreen.kt
@@ -0,0 +1,49 @@
+package com.example.meditationui.ui.theme
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.meditationui.R
+
+@Composable
+fun HomeScreen() {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(DeepBlue)) {
+        Column {
+            GreetingSection(name = "Prerit")
+        }
+    }
+}
+
+@Composable
+fun GreetingSection(name: String) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+    ) {
+        Column(verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "Good Morning, $name", 
+                style = MaterialTheme.typography.h4)
+            Text(text = "We wish you to have a good day",
+                style = MaterialTheme.typography.body1)
+        }
+        Icon(painter = painterResource(id = R.drawable.ic_search),
+            contentDescription = "Search",
+            tint = Color.White,
+            modifier = Modifier.size(24.dp)
+        )
+    }
+}
