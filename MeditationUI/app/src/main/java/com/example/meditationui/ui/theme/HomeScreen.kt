0c001f4b31388056a75a4eb4325eee520cecda63
preritjn <preritbgr3010@gmail.com>
2023-01-08 23:33:42 +0530

    Added clips

--- MeditationUI/app/src/main/java/com/example/meditationui/ui/theme/HomeScreen.kt
+++ MeditationUI/app/src/main/java/com/example/meditationui/ui/theme/HomeScreen.kt
@@ -1,13 +1,21 @@
 package com.example.meditationui.ui.theme
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -20,6 +28,7 @@ fun HomeScreen() {
         .background(DeepBlue)) {
         Column {
             GreetingSection(name = "Prerit")
+            ChipSection(chips = listOf("Madarchod","Randi","Dari","Betichod","Chuttadchod","Bala","Maghiya"))
         }
     }
 }
@@ -36,14 +45,48 @@ fun GreetingSection(name: String) {
         Column(verticalArrangement = Arrangement.Center
         ) {
             Text(text = "Good Morning, $name", 
-                style = MaterialTheme.typography.h4)
-            Text(text = "We wish you to have a good day",
-                style = MaterialTheme.typography.body1)
+                style = MaterialTheme.typography.h4,
+            color = Color.White)
+            Text(text = "We wish you to have a good day!!",
+                style = MaterialTheme.typography.body1,
+            color = TextWhite)
         }
         Icon(painter = painterResource(id = R.drawable.ic_search),
             contentDescription = "Search",
             tint = Color.White,
             modifier = Modifier.size(24.dp)
         )
+    }
+}
+
+@Composable
+fun ChipSection(
+    chips: List<String>
+) {
+    var selectedChipIndex by remember {
+        mutableStateOf(0)
+    }
+    LazyRow {
+        items(chips.size) {
+            Box(contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .padding(start = 15.dp, top = 15.dp, bottom = 15.dp)
+                    .clickable {
+                        selectedChipIndex = it
+                    }
+                    .clip(RoundedCornerShape(15.dp))
+                    .background(
+                        if (selectedChipIndex == it)
+                            ButtonBlue
+                        else
+                            DarkerButtonBlue
+                    )
+                    .padding(15.dp)
+            )
+            {
+                Text(text = chips[it],
+                    color = TextWhite)
+            }
+        }
     }
 }
